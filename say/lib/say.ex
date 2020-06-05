1175d64c12a2425f9760b9cf6115c909456f642d
Bradley <blewfargs@gmail.com>
2020-06-05 15:48:06 -0500

    completed say (which outputs a text version of any integer from 0 to 1 trillion exclusive), also included the optional enunciation capable (tells the shell of the outside machine to run espeak on that text)

--- say/lib/say.ex
+++ say/lib/say.ex
@@ -1,91 +1,77 @@
 defmodule Say do
   defguard is_billion(val) when val >= 1_000_000_000
   defguard is_million(val) when val >= 1_000_000
-  defguard is_thousand(val) when val >= 1_000
-  defguard is_hundred(val) when val >= 100
-  defguard is_ty(val) when val >= 20
-  defguard is_teen(val) when val >= 10
-  defguard is_literal(val) when val >= 1
-  defguard is_zero(val) when val == 0
+  defguard is_thousand(val) when val >= 1_000 and val < 1_000_000
+  defguard is_hundred(val) when val >= 100 and val < 1_000
+  defguard is_ty(val) when val >= 20 and val < 100
+  defguard is_teen(val) when val >= 10 and val < 20
+  defguard is_literal(val) when val >= 1 and val < 10
   defguard is_negative(val) when val < 0
-  defguard is_too_big(val) when val > 999_999_999_999 or val < -999_999_999_999
+  defguard is_too_big(val) when val > 999_999_999_999
+
+  @doc """
+  For enunciate to work, the System outside of elixir will need to have
+  espeak installed (on some linux systems, running `sudo apt-get espeak`
+  should do the trick)
+
+  Enunciate takes a range or integer and tells the outside operating system
+  to say that integer or that range.
+  """
+  def enunciate(first .. last) do
+    for i <- first .. last, do: enunciate(i)
+  end
+
+  def enunciate(number) do
+    case in_english(number) do
+      {:ok, text} -> System.cmd("espeak", [text])
+      {:error, error_text} -> System.cmd("espeak", ["error! " <> error_text])
+    end
+  end
 
   @doc """
   Translate a positive integer into English.
   """
-  @spec translate(integer) :: {atom, String.t()}
+  @spec in_english(integer) :: {atom, String.t()}
+  def in_english(num) when not is_integer(num), do: {:error, "not an integer"}
+  def in_english(num) when is_negative(num) or is_too_big(num), do: {:error, "number is out of range"}
+  def in_english(0), do: {:ok, "zero"}
+  def in_english(num) when is_integer(num) do
+    result = num |> chunkify() |> scale_words()
 
-  def in_english(number) when not is_integer(number) do
-    {:error, "not an integer"}
+    {:ok, result}
   end
 
-  def in_english(num) when is_integer(number) do
-    cond do
-      Integer.digits(num) > 12 -> {:error, "number is too large"}
-      true -> {:ok, translate(number)}
-    end
+  defp chunkify(number) do
+    number
+    |> Integer.digits()
+    |> Enum.reverse()
+    |> Enum.chunk_every(3)
+    |> Enum.map(&Enum.reverse/1)
+    |> Enum.reverse()
+    |> Enum.map(&Integer.undigits/1)
   end
 
-  def in_english(0) do
-    {:ok, "zero"}
-  end
+  def scale_words([first | [0]]), do: translate(first) <> " thousand"
 
-  def translate(num) do
-    case Integer.digits(num)
-      1 -> literal(num)
-      2 -> meld_tens(num)
-      3 -> meld_hundred(num)
-      4 -> "thou"
-      5 -> "tenthou"
-      6 -> "hundred thou"
-      7 -> "mil"
-      8 -> "ten mil"
-      9 -> "hundred mil"
-      10 -> "bil"
-      11 -> "ten bil"
-      12 -> "hundred bil"
+  def scale_words([first | [0, 0]]), do: translate(first) <> " million"
+
+  def scale_words([first | [0, 0, 0]]), do: translate(first) <> " billion"
+
+  def scale_words([first | rest] = chunks) do
+    case length(chunks) do
+      4 -> translate(first) <> " billion " <> scale_words(rest)
+      3 -> translate(first) <> " million " <> scale_words(rest)
+      2 -> translate(first) <> " thousand " <> scale_words(rest)
+      1 -> translate(first)
     end
   end
   
-  def translate(number) when is_negative(number), do: "negative " <> translate(number * -1)
-  def translate(number) when is_billion(number), do: meld_billion(number)
-  def translate(number) when is_million(number), do: meld_million(number)
-  def translate(number) when is_thousand(number), do: meld_thousand(number)
   def translate(number) when is_hundred(number), do: meld_hundred(number)
   def translate(number) when is_ty(number), do: meld_ty(number)
-  def translate(number) when is_teen(number), do: meld_teen(number)
+  def translate(number) when is_teen(number), do: meld_tens(number)
   def translate(number) when is_literal(number), do: literal(number)
+  def translate(0), do: ""
 
-  defp digit_at_place(num, place), do: Integer.digits(num) |> List.reverse() |> List.at(place)
-
-  defp zeros_place(num), do: digit_at_place(num, 0)
-  defp tens_place(num), do: digit_at_place(num, 1)
-  defp hundreds_place(num), do: digit_at_place(num, 2)
-  defp thousands_place(num), do: digit_at_place(num, 3)
-  defp ten_thousands_place(num), do: digit_at_place(num, 4)
-  defp hundred_thousands_place(num), do: digit_at_place(num, 5)
-  defp millions_place(num), do: digit_at_place(num, 6)
-  defp ten_millions_place(num), do: digit_at_place(num, 7)
-  defp hundred_millions_place(num), do: digit_at_place(num, 8)
-  defp billions_place(num), do: digit_at_place(num, 9)
-  defp ten_billions_place(num), do: digit_at_place(num, 10)
-  defp hundred_billions_place(num), do: digit_at_place(num, 11)
-
-
-  defp zero_in_ones_place?(num), do: zeros_place(num) == 0
-  defp zero_in_tens_place?(num), do: tens_place(num) == 0
-  defp zero_in_hundreds_place?(num), do: hundreds_place(num) == 0
-  defp zero_in_thousands_place?(num), do: thousands_place(num) == 0
-  defp zero_in_ten_thousands_place?(num), do: ten_thousands_place(num) == 0
-  defp zero_in_hundred_thousands_place?(num), do: hundred_thousands_place(num) == 0
-  defp zero_in_millions_place?(num), do: millions_place(num) == 0
-  defp zero_in_ten_millions_place?(num), do: ten_millions_place(num) == 0
-  defp zero_in_hundred_millions_place?(num), do: hundred_millions_place(num) == 0
-  defp zero_in_billions_place?(num), do: billions_place(num) == 0
-  defp zero_in_ten_billions_place?(num), do: ten_billions_place(num) == 0
-  defp zero_in_hundred_billions_place?(num), do: hundred_billions_place(num) == 0
-
-  def literal(0), do: ""
   def literal(1), do: "one"
   def literal(2), do: "two"
   def literal(3), do: "three"
@@ -103,66 +89,36 @@ defmodule Say do
   defp meld_tens(15), do: "fifteen"
   defp meld_tens(18), do: "eighteen"
   defp meld_tens(num) when is_teen(num), do: translate(num - 10) <> "teen"
+  defp meld_tens(num) when is_ty(num), do: meld_ty(num)
 
-  # defp biggest_tens_place(number) do
-  # end
 
-  defp meld_ty(90), do: "ninety"
-  defp meld_ty(num) when num > 90, do: translate(90) <> "-" <> translate(num - 90)
-  defp meld_ty(80), do: "eighty"
-  defp meld_ty(num) when num > 80, do: translate(80) <> "-" <> translate(num - 80)
-  defp meld_ty(70), do: "seventy"
-  defp meld_ty(num) when num > 70, do: translate(70) <> "-" <> translate(num - 70)
-  defp meld_ty(60), do: "sixty"
-  defp meld_ty(num) when num > 60, do: translate(60) <> "-" <> translate(num - 60)
-  defp meld_ty(50), do: "fifty"
-  defp meld_ty(num) when num > 50, do: translate(50) <> "-" <> translate(num - 50)
-  defp meld_ty(40), do: "forty"
-  defp meld_ty(num) when num > 40, do: translate(40) <> "-" <> translate(num - 40)
-  defp meld_ty(30), do: "thirty"
-  defp meld_ty(num) when num > 30, do: translate(30) <> "-" <> translate(num - 30)
   defp meld_ty(20), do: "twenty"
-  defp meld_ty(num) when num > 20, do: translate(20) <> "-" <> translate(num - 20)  
+  defp meld_ty(30), do: "thirty"
+  defp meld_ty(40), do: "forty"
+  defp meld_ty(50), do: "fifty"
+  defp meld_ty(80), do: "eighty"
+  defp meld_ty(num) do
+    cond do
+      rem(num, 10) == 0 ->
+        ten_magnigtude = num / 10 |> round() |> translate()
+        ten_magnigtude <> "ty"
+
+      true ->
+        tens_num = rem(num, 10) |> round()
+        translate(num - tens_num) <> "-" <> translate(tens_num)
+    end
+  end
 
   defp meld_hundred(num) do
-    if zero_in_ones_place?(num) and zero_in_tens_place?(num) do
-      hundreds_place(num) <> " " <> "hundred"
-    else
-      translate(hundreds_place(num) * 100) <> " " <> translate(just_below_hundreds(num))
+    cond do
+      rem(num, 100) == 0 ->
+        hundred_magnitude = num / 100 |> round() |> translate()
+        hundred_magnitude <> " hundred"
+
+      true ->
+        hundreds_num = rem(num, 100) |> round()
+        translate(num - hundreds_num) <> " " <> translate(hundreds_num)
+
     end
   end
-
-  defp meld_thousand(num) do
-    if zero_in_ones_place?() and zero_in_tens_place?() and zero_in_hundreds_place?() do
-      thousands_place(num) <> " " <> "thousand"
-    else
-      translate(thousands_place(num) * 1_000) <> " " <> translate(just_below_thousands(num))
-    end
-  end
-
-  defp meld_ten(num) do
-    if zero_in_ones_place?() and zero_in_tens_place?() and zero_in_hundreds_place?() do
-      "big ten-thousandsNumber"
-    else
-      translate(ten_thousands_place(num) * 10_000) <> " " <> translate(just_below_ten_thousands(num))
-    end
-  end
-
-  defp meld_million(1), do: ""
-  defp meld_million(1), do: ""
-  defp meld_million(1), do: ""
-  defp meld_million(1), do: ""
-  defp meld_million(1), do: ""
-  defp meld_million(1), do: ""
-  defp meld_million(1), do: ""
-  defp meld_million(1), do: ""
-  defp meld_million(1), do: ""
-  defp meld_million(1), do: ""
-  defp meld_million(1), do: ""
-
-  defp meld_billion(1_000_000_000), do: "billion"
-  defp meld_billion(number), do: translate(number - 1_000_000_000) <> meld_billion(1_000_000_000)
-
-
-
 end
--- say/test/say_test.exs
+++ say/test/say_test.exs
@@ -1,6 +1,15 @@
 defmodule SayTest do
   use ExUnit.Case
 
+  # test "chunk (now a private function)" do
+  #   assert Say.chunkify(1_234_567_890) == [1, 234, 567, 890]
+  #   assert Say.chunkify(100) == [100]
+  #   assert Say.chunkify(1_000) == [1, 0]
+  #   assert Say.chunkify(90_000) == [90, 0]
+  #   assert Say.chunkify(90_090) == [90, 90]
+  #   assert Say.chunkify(90_900) == [90, 900]
+  # end
+
   # @tag :pending
   test "zero" do
     assert Say.in_english(0) == {:ok, "zero"}
@@ -11,9 +20,25 @@ defmodule SayTest do
     assert Say.in_english(1) == {:ok, "one"}
   end
 
+  test "10" do
+    assert Say.in_english(10) == {:ok, "ten"}
+  end
+
+  test "12" do
+    assert Say.in_english(12) == {:ok, "twelve"}
+  end
+
   # @tag :pending
   test "fourteen" do
     assert Say.in_english(14) == {:ok, "fourteen"}
+  end
+
+  test "15" do
+    assert Say.in_english(15) == {:ok, "fifteen"}
+  end
+
+  test "18" do
+    assert Say.in_english(18) == {:ok, "eighteen"}
   end
 
   # @tag :pending
@@ -26,9 +51,29 @@ defmodule SayTest do
     assert Say.in_english(22) == {:ok, "twenty-two"}
   end
 
+  test "28" do
+    assert Say.in_english(28) == {:ok, "twenty-eight"}
+  end
+
+  test "50" do
+    assert Say.in_english(50) == {:ok, "fifty"}
+  end
+
+  test "98" do
+    assert Say.in_english(98) == {:ok, "ninety-eight"}
+  end
+
   # @tag :pending
   test "one hundred" do
     assert Say.in_english(100) == {:ok, "one hundred"}
+  end
+
+  test "101" do
+    assert Say.in_english(101) == {:ok, "one hundred one"}
+  end
+
+  test "103" do
+    assert Say.in_english(103) == {:ok, "one hundred three"}
   end
 
   # @tag :pending
@@ -36,44 +81,60 @@ defmodule SayTest do
     assert Say.in_english(123) == {:ok, "one hundred twenty-three"}
   end
 
-  @tag :pending
+  test "509" do
+    assert Say.in_english(509) == {:ok, "five hundred nine"}
+  end
+
+  test "800" do
+    assert Say.in_english(800) == {:ok, "eight hundred"}
+  end
+
+  test "828" do
+    assert Say.in_english(828) == {:ok, "eight hundred twenty-eight"}
+  end
+
+  test "990" do
+    assert Say.in_english(990) == {:ok, "nine hundred ninety"}
+  end
+
+  # @tag :pending
   test "one thousand" do
     assert Say.in_english(1_000) == {:ok, "one thousand"}
   end
 
-  @tag :pending
+  # @tag :pending
   test "one thousand two hundred thirty-four" do
     assert Say.in_english(1_234) == {:ok, "one thousand two hundred thirty-four"}
   end
 
-  @tag :pending
+  # @tag :pending
   test "one million" do
     assert Say.in_english(1_000_000) == {:ok, "one million"}
   end
 
-  @tag :pending
+  # @tag :pending
   test "one million two thousand three hundred forty-five" do
     assert Say.in_english(1_002_345) == {:ok, "one million two thousand three hundred forty-five"}
   end
 
-  @tag :pending
+  # @tag :pending
   test "one billion" do
     assert Say.in_english(1_000_000_000) == {:ok, "one billion"}
   end
 
-  @tag :pending
+  # @tag :pending
   test "a big number" do
     assert Say.in_english(987_654_321_123) ==
              {:ok,
               "nine hundred eighty-seven billion six hundred fifty-four million three hundred twenty-one thousand one hundred twenty-three"}
   end
 
-  @tag :pending
+  # @tag :pending
   test "numbers below zero are out of range" do
     assert Say.in_english(-1) == {:error, "number is out of range"}
   end
 
-  @tag :pending
+  # @tag :pending
   test "numbers above 999,999,999,999 are out of range" do
     assert Say.in_english(1_000_000_000_000) == {:error, "number is out of range"}
   end
