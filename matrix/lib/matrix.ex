4605b411d0450adad488ba8a31403f76b5eeb2ec
Bradley <blewfargs@gmail.com>
2020-06-04 22:12:32 -0500

    completed a first iteration of matrix

--- matrix/lib/matrix.ex
+++ matrix/lib/matrix.ex
@@ -1,6 +1,9 @@
 defmodule Matrix do
   defstruct matrix: nil
 
+  defguardp starts_col(col) when col == 0
+  defguardp not_first_row(row) when row != 0
+
   @doc """
   Convert an `input` string, with rows separated by 
   newlines and values
@@ -8,26 +11,27 @@ defmodule Matrix do
   """
   @spec from_string(input :: String.t()) :: %Matrix{}
   def from_string(input) do
-    rows = Enum.to_list(0..String.length(input) - 1)
+    k = String.length(input) - 1
+    rows = Enum.to_list(0..k)
 
-    split_by_row = String.split(input, ~s/\n/) |> Enum.map(&String.split()/1)
+    split_by_row = String.split(input, ~s/\n/) |> Enum.map(&String.split/1)
     List.zip([rows, split_by_row])
-    |> Enum.map(fn {row_index, list_of_row} = rows  ->
-      cols = Enum.to_list(0..length(list_of_row) - 1)
-      
-      custom_zip(row_index, cols, list_of_row)
-    end)
+    |> Enum.map(&apply_columns/1)
     |> List.flatten()
     |> Enum.into(%{})
-    # |> Enum.reduce(&(Map.merge(&1, &2, fn _k, v1, v2 -> v1 + v2 end ) ) )
-    # |> Enum.into(%{})
   end
 
-  defp custom_zip(row_index, cols, list_of_row) do
-    columns = List.zip([cols, list_of_row])
-    for column_index <- cols do {{row_index, column_index}, Enum.at(list_of_row, column_index)} end
+  defp apply_columns({row_index, list_of_rows}) do
+    j = length(list_of_rows) - 1
+    cols = Enum.to_list(0..j)
 
-    # {row_index, cols, list_of_row}
+    custom_zip(row_index, cols, list_of_rows)
+  end
+
+  defp custom_zip(row_index, cols, list_of_row) do    
+    for column_index <- cols do
+      {{row_index, column_index}, Enum.at(list_of_row, column_index)}
+    end
   end
 
   @doc """
@@ -40,16 +44,29 @@ defmodule Matrix do
     list_o_tuples = Map.to_list(matrix)
 
     list_o_tuples
-    |> Enum.map(fn
-      {{row, col}, value} when col == 0 and row != 0-> {{row, col}, "\n" <> value}
-      {{row, col}, value} -> {{row, col}, value} 
-    end)
-    |> Enum.reduce([], fn
-      {{_row, 0}, value}, acc -> acc ++ [value]
-      {{_row, _col}, value}, acc -> acc ++ [" " <> value]
-      end)
+    |> Enum.map(&add_newlines/1)
+    |> Enum.reduce([], &add_spaces/2)
     |> Enum.join()
+  end
 
+  defp add_newlines({{row, col}, value}) when starts_col(col) and not_first_row(row) do
+    {{row, col}, "\n" <> value}
+  end
+  defp add_newlines({{_row, _col}, _value} = mapping_tuple), do: mapping_tuple
+
+  defp add_spaces({{_row, 0}, value}, acc), do: acc ++ [value]
+  defp add_spaces({{_row, _col}, value}, acc), do: acc ++ [" " <> value]
+
+  @doc """
+  Given a `matrix` and `index`, return the row at `index`.
+  """
+  @spec row(matrix :: %Matrix{}, index :: integer) :: list(integer)
+  def row(matrix, index) do
+    matrix
+    |> Map.to_list()
+    |> Enum.filter(fn {{row, _col}, _val} = _mapping_tuple -> row == index end)
+    |> Enum.map(fn {{_row, _col}, val} -> val end)
+    |> Enum.map(&String.to_integer/1)
   end
 
   @doc """
@@ -58,37 +75,12 @@ defmodule Matrix do
   """
   @spec rows(matrix :: %Matrix{}) :: list(list(integer))
   def rows(matrix) do
-    positions_to_values = Map.to_list(matrix)
     positions = Map.keys(matrix)
     biggest_row = positions
     |> Enum.map(fn {row, _col} -> row end)
     |> Enum.max()
-    biggest_col = positions
-    |> Enum.map(fn {_row, col} -> col end)
-    |> Enum.max()
-    
-    positions_to_val    positions = Map.keys(matrix)
-    biggest_row = positions
-    |> Enum.map(fn {row, _col} -> row end)
-    |> Enum.max()
-    biggest_col = positions
-    |> Enum.map(fn {_row, col} -> col end)
-    |> Enum.max()ues
-  end
 
-  @doc """
-  Given a `matrix` and `index`, return the row at `index`.
-  """
-  @spec row(matrix :: %Matrix{}, index :: integer) :: list(integer)
-  def row(matrix, index) do
-  end
-
-  @doc """
-  Given a `matrix`, return its columns as a
-  list of lists of integers.
-  """
-  @spec columns(matrix :: %Matrix{}) :: list(list(integer))
-  def columns(matrix) do
+    for i <- 0 .. biggest_row, do: row(matrix, i)
   end
 
   @doc """
@@ -97,5 +89,24 @@ defmodule Matrix do
   """
   @spec column(matrix :: %Matrix{}, index :: integer) :: list(integer)
   def column(matrix, index) do
+    matrix
+    |> Map.to_list()
+    |> Enum.filter(fn {{_row, col}, _val} -> col == index end)
+    |> Enum.map(fn {{_row, _col}, val} -> val end)
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  @doc """
+  Given a `matrix`, return its columns as a
+  list of lists of integers.
+  """
+  @spec columns(matrix :: %Matrix{}) :: list(list(integer))
+  def columns(matrix) do
+    positions = Map.keys(matrix)
+    biggest_col = positions
+    |> Enum.map(fn {_row, col} -> col end)
+    |> Enum.max()
+
+    for i <- 0 .. biggest_col, do: column(matrix, i)
   end
 end
--- matrix/test/matrix_test.exs
+++ matrix/test/matrix_test.exs
@@ -2,26 +2,49 @@ defmodule MatrixTest do
   use ExUnit.Case
 
   @input "1 2 3\n4 5 6\n7 8 9"
+  @input_incomplete_line "3 9 1\n0 6 7\n4"
+  @matrix_incomplete_line %{
+    {0,0} => "3",
+    {0,1} => "9",
+    {0,2} => "1",
+    {1,0} => "0",
+    {1,1} => "6",
+    {1,2} => "7",
+    {2,0} => "4",
+}
 
   test "from_string" do
-    assert Matrix.from_string(@input) == 
-    %{
+    assert Matrix.from_string(@input) == %{
       {0,0} => "1",
-    {0,1} => "2",
-    {0,2} => "3",
-    {1,0} => "4",
-    {1,1} => "5",
-    {1,2} => "6",
-    {2,0} => "7",
-    {2,1} => "8",
-    {2,2} => "9"
-  }
+      {0,1} => "2",
+      {0,2} => "3",
+      {1,0} => "4",
+      {1,1} => "5",
+      {1,2} => "6",
+      {2,0} => "7",
+      {2,1} => "8",
+      {2,2} => "9"
+    }
+    assert Matrix.from_string(@input_incomplete_line) == @matrix_incomplete_line
+  end
+
+  test "to_string" do
+    assert Matrix.to_string(@matrix_incomplete_line) == @input_incomplete_line
   end
 
   # @tag :pending
   test "reading from and writing to string" do
     matrix = Matrix.from_string(@input)
     assert Matrix.to_string(matrix) == @input
+  end
+
+  # @tag :pending
+  test "row should return list at index" do
+    matrix = Matrix.from_string(@input)
+
+    assert Matrix.row(matrix, 0) == [1, 2, 3]
+    assert Matrix.row(matrix, 1) == [4, 5, 6]
+    assert Matrix.row(matrix, 2) == [7, 8, 9]
   end
 
   # @tag :pending
@@ -35,16 +58,16 @@ defmodule MatrixTest do
            ]
   end
 
-  @tag :pending
-  test "row should return list at index" do
+  # @tag :pending
+  test "column should return list at index" do
     matrix = Matrix.from_string(@input)
 
-    assert Matrix.row(matrix, 0) == [1, 2, 3]
-    assert Matrix.row(matrix, 1) == [4, 5, 6]
-    assert Matrix.row(matrix, 2) == [7, 8, 9]
+    assert Matrix.column(matrix, 0) == [1, 4, 7]
+    assert Matrix.column(matrix, 1) == [2, 5, 8]
+    assert Matrix.column(matrix, 2) == [3, 6, 9]
   end
 
-  @tag :pending
+  # @tag :pending
   test "columns should return nested lists regardless of internal structure" do
     matrix = Matrix.from_string(@input)
 
@@ -53,14 +76,5 @@ defmodule MatrixTest do
              [2, 5, 8],
              [3, 6, 9]
            ]
-  end
-
-  @tag :pending
-  test "column should return list at index" do
-    matrix = Matrix.from_string(@input)
-
-    assert Matrix.column(matrix, 0) == [1, 4, 7]
-    assert Matrix.column(matrix, 1) == [2, 5, 8]
-    assert Matrix.column(matrix, 2) == [3, 6, 9]
   end
 end
