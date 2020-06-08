2d0c7cf3bb8c96cd5c638a0bb34b4bbcf6df83ea
Bradley <blewfargs@gmail.com>
2020-06-08 16:55:10 -0500

    finished a first version of bank-account, which delves into concurrency, using an adhoc setup involving Agents

--- bank-account/lib/bank_account.ex
+++ bank-account/lib/bank_account.ex
@@ -3,6 +3,8 @@ defmodule BankAccount do
   A bank account that supports access from multiple processes.
   """
 
+  @initial_value 0
+
   @typedoc """
   An account handle.
   """
@@ -13,26 +15,42 @@ defmodule BankAccount do
   """
   @spec open_bank() :: account
   def open_bank() do
+    {:ok, account} = Agent.start(fn -> @initial_value end)
+
+    account
   end
 
   @doc """
   Close the bank. Makes the account unavailable.
   """
   @spec close_bank(account) :: none
-  def close_bank(account) do
+  def close_bank(pid) do
+    Agent.update(pid, fn _ -> :account_closed end)
   end
 
   @doc """
   Get the account's balance.
   """
   @spec balance(account) :: integer
-  def balance(account) do
+  def balance(pid) do
+    tried = Agent.get(pid, &(&1))
+
+    case tried do
+      :account_closed -> {:error, :account_closed}
+      _ -> tried
+    end
   end
 
   @doc """
   Update the account's balance by adding the given amount which may be negative.
   """
   @spec update(account, integer) :: any
-  def update(account, amount) do
+  def update(pid, amount) do
+    tried = Agent.get(pid, &(&1))
+
+    case tried do
+      :account_closed -> {:error, :account_closed}
+      _ -> Agent.update(pid, &(&1 + amount))
+    end
   end
 end
