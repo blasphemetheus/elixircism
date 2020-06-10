3a4263d02c45749e794e7c52398d12e7a030b011
Bradley <blewfargs@gmail.com>
2020-06-10 01:46:26 -0500

    completed grains, an exponential growth problem, it's funny how there's no elixir-specific power function but it's easy enough to make (there is an erlang math library one but that's no fun)

--- grains/lib/grains.ex
+++ grains/lib/grains.ex
@@ -3,7 +3,15 @@ defmodule Grains do
   Calculate two to the power of the input minus one.
   """
   @spec square(pos_integer) :: pos_integer
-  def square(number) do
+  def square(1), do: {:ok, 1}
+  def square(2), do: {:ok, 2}
+  def square(num) when num <= 64 and num >= 1 do
+    {:ok, prev} = square(num - 1)
+
+    {:ok, prev * 2}
+  end
+  def square(num) do
+    {:error, "The requested square must be between 1 and 64 (inclusive)"}
   end
 
   @doc """
@@ -11,5 +19,10 @@ defmodule Grains do
   """
   @spec total :: pos_integer
   def total do
+    list_of_squares = for squ <- 1 .. 64, do: square(squ) |> extract()
+
+    {:ok, list_of_squares |> Enum.sum()}
   end
+
+  defp extract({:ok, result}), do: result
 end
