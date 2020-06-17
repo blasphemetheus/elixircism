8e26b2e8f0da880c7bec8346835424f9a55d7ad6
Bradley <blewfargs@gmail.com>
2020-06-17 12:45:03 -0500

    finished this problem, simple reversing of a map which uses lists to record the former values

--- etl/lib/etl.ex
+++ etl/lib/etl.ex
@@ -9,5 +9,20 @@ defmodule ETL do
   """
   @spec transform(map) :: map
   def transform(input) do
+    input
+    |> Enum.map(&reverse/1)
+    |> Enum.reduce([], &pull_out_from_list/2)
+    |> Enum.map(&old_to_new/1)
+    |> Enum.reduce(%{}, fn {val, key}, acc -> Map.merge(acc, %{val => key}) end)
+  end
+
+  defp reverse({key, val}), do: {val, key}
+
+  defp pull_out_from_list({list, val}, acc) when is_list(list) do
+    acc ++ for i <- list, do: {i, val}
+  end
+
+  defp old_to_new({old_word, old_val}) do
+    {String.downcase(old_word), old_val}
   end
 end
